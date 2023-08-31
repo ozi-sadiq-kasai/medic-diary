e6420e1852e298d0310260d65e31903c044f7f00
ozi <ozisadiqkasai@gmail.com>
2023-08-31 19:07:22 +0100

    modified files

--- src/Component/Common/Loader.jsx
+++ src/Component/Common/Loader.jsx
@@ -1,15 +1,17 @@
+
+
+
 import { Space, Spin } from 'antd';
-import "../../Sass/Loader.scss"
+import "../../Sass/Loader.scss";
+
 export default function Loader() {
- return(
-  <div className="loader">
-   <p>Loading... Please Wait...</p>
-    <Space size="middle"/>
-    <Spin size="large"/>
-    <Space/>
-  </div>
-)
+  return (
+    <div className="loader">
+      <p>Loading... Please Wait...</p>
+      <Space size="middle"/>
+      <Spin size="large" className="custom-spin" />
+      <Space/>
+    </div>
+  );
 }
-
-
 
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -20,6 +20,7 @@ export default function Profile({ currentUser }) {
     });
   }, []);
   return loading ? <Loader />  : <ProfileComponent currentUser={currentUser} />;
+  
  
   
   
